3f67ad73aef66e6ef7bbb869f2b706605f02eb30
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2024-10-30 23:54:32 +0530

    two pointer if sorted,'nested for'

--- Leetcode/Two_Sum_1LC.java
+++ Leetcode/Two_Sum_1LC.java
@@ -8,14 +8,15 @@ public class Two_Sum_1LC {
         for(int i=0;i<a.length;i++)
             System.out.print(a[i]+" ");
     }
-    public static int[] sumtarg(int[] arr,int tar){
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==tar){
-                    return new int[]{i,j};
+    public static int[] sumtarg(int[] nums,int target){
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] + nums[j - i] == target) {
+                    return new int[] { j, j - i };
                 }
             }
         }
         return null;
+        //if the array is sorted we can easily apply the binary search two pointer approach.
     }
 }
