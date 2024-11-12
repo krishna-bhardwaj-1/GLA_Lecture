ff4b7744927429674bdc768dc6c93dddf8b85fde
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2024-11-12 23:14:24 +0530

    binary search return closest

--- Leetcode/Valid_perfect_sqaure_367LC.java
+++ Leetcode/Valid_perfect_sqaure_367LC.java
@@ -6,28 +6,21 @@ public class Valid_perfect_sqaure_367LC {
         System.out.println(isPerfectSquare(num));
     }
     public static boolean isPerfectSquare(int num) {
-        if(num==1||num==4||num==9||num==16||num==25||num==36||num==49) {
-            return true;
-        }
-        int[] validsquare=new int[(int) Math.sqrt(num)];
-        for(int i=0;i<validsquare.length;i++){
-            validsquare[i]=i+1;
-        }
-        int lo=0;
-        int hi=validsquare.length-1;
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int e=validsquare[mid]*validsquare[mid];
-            if(e==num){
-                return true;
+            if(num<1) return false;
+            long lo=1;
+            long hi=num;
+            while(lo<=hi){
+                long mid=(lo+hi)/2;
+                if(mid*mid==num){
+                    return true;
+                }
+                else if(mid*mid>num){
+                    hi=mid-1;
+                }
+                else{
+                    lo=mid+1;
+                }
             }
-            else if(e<num){
-                lo=mid+1;
-            }
-            else {
-                hi=mid-1;
-            }
-        }
-        return false;
+            return false;
     }
 }
