272aea0ae6c4a4ce0f01b0918d8f392796871e7a
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2024-11-19 08:22:05 +0530

    sort and compare indexes

---
+++ Leetcode/Height_checker.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class Height_checker {
+    public static void main(String[] args) {
+        int[] heights={1,1,4,2,1,3};
+        heightChecker(heights);
+    }
+    public static void heightChecker(int[] heights) {
+        int[] arr= Arrays.copyOf(heights,heights.length);
+        Arrays.sort(arr);
+        int c=0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=arr[i]){
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
