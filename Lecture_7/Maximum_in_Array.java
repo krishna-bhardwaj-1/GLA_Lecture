a7519d58dd9a3588640e47ac421a8b3a2e29f07b
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2024-12-05 23:10:57 +0530

    kadens

--- Lecture_7/Maximum_in_Array.java
+++ Lecture_7/Maximum_in_Array.java
@@ -2,7 +2,7 @@ package Lecture_7;
 
 public class Maximum_in_Array {
     public static void main(String[] args) {
-        int[] arr={2,3,4,1,7,9,8};
+        int[] arr={209,3,4,1,7,9,8};
         System.out.println(Maximum2(arr));
     }
     public static int Maximum1(int[] arr){
@@ -16,7 +16,7 @@ public class Maximum_in_Array {
     }
     public static int Maximum2(int[] arr){
         int max=Integer.MIN_VALUE;  //-2^31
-        for(int i=1;i<arr.length;i++){
+        for(int i=0;i<arr.length;i++){
             max=Math.max(max,arr[i]);
         }
         return max;
