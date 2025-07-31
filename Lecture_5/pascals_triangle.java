e8c24af6d23f05e18aec54a648eb3f50d92fc455
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-07-31 23:47:07 +0530

    Pascal Triangle

--- Lecture_5/pascals_triangle.java
+++ Lecture_5/pascals_triangle.java
@@ -1,6 +1,8 @@
 package Lecture_5;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class pascals_triangle {
     public static void main(String[] args) {
@@ -8,12 +10,12 @@ public class pascals_triangle {
         int n=sc.nextInt();
         int star=1;
         int row=0;
-        while(row<n){
-            int i=0;
-            int ncr=1;
-            while(i<star){
-                System.out.print(ncr+" ");
-                ncr=((row-i)*ncr)/(i+1);
+        while(row<n) {
+            int i = 0;
+            int ncr = 1;
+            while (i < star) {
+                System.out.print(ncr + " ");
+                ncr = ((row - i) * ncr) / (i + 1);
                 i++;
             }
             star++;
